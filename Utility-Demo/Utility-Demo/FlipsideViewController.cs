0496dd8e8bbf9fb0d45505ea4badab56e48ef12a
Aaron He <hwaaron@live.com>
2012-03-19 02:25:53 -0400

    Editing utlity demo

--- Utility-Demo/Utility-Demo/FlipsideViewController.cs
+++ Utility-Demo/Utility-Demo/FlipsideViewController.cs
@@ -4,10 +4,13 @@ using System.Drawing;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
+using UtilityDemo.www.w3schools.com;
+
 namespace UtilityDemo
 {
 	public partial class FlipsideViewController : UIViewController
 	{
+		private TempConvert converter;
 		public FlipsideViewController () : base ("FlipsideViewController", null)
 		{
 		}
@@ -17,6 +20,46 @@ namespace UtilityDemo
 			base.ViewDidLoad ();
 			
 			// Perform any additional setup after loading the view, typically from a nib.
+			resultLabel.Text = "";
+			activityIndicator.HidesWhenStopped = true;
+			converter = new TempConvert();
+			convertButton.TouchUpInside += HandleConvertButtonTouchUpInside;
+			convertButton.TouchUpInside += AnyNonTextControlTouched;
+			conversionSelectionButton.ValueChanged += AnyNonTextControlTouched;
+		}
+		
+		private void AnyNonTextControlTouched(object sender, EventArgs e)
+		{
+			temperatureTextBox.ResignFirstResponder();
+		}
+
+		void HandleConvertButtonTouchUpInside (object sender, EventArgs e)
+		{
+			resultLabel.Text="";
+			double inputTemp;
+			if(double.TryParse(temperatureTextBox.Text, out inputTemp))
+			{
+				UIApplication.SharedApplication.NetworkActivityIndicatorVisible =true;
+				activityIndicator.StartAnimating();
+				if(conversionSelectionButton.SelectedSegment==0)
+					converter.BeginCelsiusToFahrenheit(temperatureTextBox.Text, FinishedConversion, true);
+				else
+					converter.BeginFahrenheitToCelsius(temperatureTextBox.Text, FinishedConversion, false);
+			}
+			else
+				resultLabel.Text = "Invalid input temperature";
+		}
+		
+		private void FinishedConversion(IAsyncResult asyncResult)
+		{
+			var fromCelsius = (bool)asyncResult.AsyncState;
+			var answer = fromCelsius ? converter.EndCelsiusToFahrenheit(asyncResult) :converter.EndFahrenheitToCelsius(asyncResult);
+			
+			InvokeOnMainThread(() => {
+				resultLabel.Text = string.Format(fromCelsius ? "{0}°C is {1:F2}°F" : "{0}°F is {1:F2}°C", temperatureTextBox.Text, Convert.ToDouble(answer));
+				UIApplication.SharedApplication.NetworkActivityIndicatorVisible = false;
+				activityIndicator.StopAnimating();
+			});
 		}
 		
 		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
--- Utility-Demo/Utility-Demo/FlipsideViewController.designer.cs
+++ Utility-Demo/Utility-Demo/FlipsideViewController.designer.cs
@@ -1,3 +1,4 @@
+// WARNING
 //
 // This file has been generated automatically by MonoDevelop to store outlets and
 // actions made in the Xcode designer. If it is removed, they will be lost.
@@ -10,12 +11,50 @@ namespace UtilityDemo
 	[Register ("FlipsideViewController")]
 	partial class FlipsideViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UITextField temperatureTextBox { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UISegmentedControl conversionSelectionButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton convertButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIActivityIndicatorView activityIndicator { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel resultLabel { get; set; }
+
 		[Action ("done:")]
 		partial void done (MonoTouch.UIKit.UIBarButtonItem sender);
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (temperatureTextBox != null) {
+				temperatureTextBox.Dispose ();
+				temperatureTextBox = null;
+			}
+
+			if (conversionSelectionButton != null) {
+				conversionSelectionButton.Dispose ();
+				conversionSelectionButton = null;
+			}
+
+			if (convertButton != null) {
+				convertButton.Dispose ();
+				convertButton = null;
+			}
+
+			if (activityIndicator != null) {
+				activityIndicator.Dispose ();
+				activityIndicator = null;
+			}
+
+			if (resultLabel != null) {
+				resultLabel.Dispose ();
+				resultLabel = null;
+			}
 		}
 	}
 }
-
